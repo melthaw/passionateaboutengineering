fa252c870ec5be522597fcd9148ec553b4be809c
marekpiotrowski <marek.piotrowski@live.com>
2017-08-06 18:13:09 +0200

    Release 1.0

--- js/app.js
+++ js/app.js
@@ -1,9 +1,50 @@
-(function() {
+$(document).ready(function() {
+    var CONTAINER_ID = '#view-container',
+        ROUTES_MAPPING = [
+        {
+            view: 'home', url: '#/home', onLoad: function() {
+                $('#go-to-stories').on('click', function() {
+                    location.hash = '#/stories';
+                });
+            }
+        },
+        {
+            view: 'stories', url: '#/stories', onLoad: function() { }
+        },
+        {
+            view: 'about', url: '#/about', onLoad: function() { }
+        }
+    ];
 
-    var app = Sammy('body');
+    loadView(getRouteFromHash(location.hash));
 
-    $(document).ready(function() {
-        app.run('#/');
+    $( window ).on('hashchange', function() {
+        var route = getRouteFromHash(location.hash);
+
+        if(!route) {
+            console.warn('No view: ' + hash);
+            return;
+        }
+        loadView(route);
     });
 
-})();
+    function getRouteFromHash(hash) {
+        var route = ROUTES_MAPPING.filter(function(mapping) {
+            return mapping.url == hash;
+        })[0];
+        if(!route)
+            route = ROUTES_MAPPING[0];
+        return route;
+    }
+
+    function loadView(route) {
+        var viewName = 'templates/' + route.view + '.html';
+        $(CONTAINER_ID).load(viewName, route.onLoad);
+    }
+
+    $('.topnav .rel').on('click', function() {
+        $('#myTopnav').prop('class', 'topnav');
+    });
+
+});
+
--- js/view-models/stories.js
+++
@@ -1,9 +0,0 @@
-(function() {
-
-    var app = Sammy.apps.body;
-
-    app.get('#/stories', function(context) {
-        console.log("You're in the stories route");
-    });
-
-})();
